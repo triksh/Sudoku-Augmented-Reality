a6781790499d5332afaa90f5958237298c624109
triksh agarwal <trikshui@gmail.com>
2016-02-10 05:32:32 -0800

    Create header.h

---
+++ header.h
@@ -0,0 +1,14 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+
+using namespace cv;
+
+int knnPrePos(Mat,Mat);
