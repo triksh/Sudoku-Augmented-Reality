253f3dae45a2a43d7de2c209a21366ee38408101
triksh agarwal <trikshui@gmail.com>
2016-02-17 10:56:43 -0800

    Update header.h

--- header.h
+++ header.h
@@ -1,3 +1,4 @@
+
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include <iostream>
@@ -5,10 +6,20 @@
 #include <stdio.h>
 #include <algorithm>
 #include <math.h>
+#include <bitset>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <ml.h>
 
 using namespace cv;
 
-int knnPrePos(Mat,Mat);
+#define FILE_NAME "F:\\sudoku1.jpg"
+#define PATH "F:\\images\\"	
+#define SX 20		// dimentions, you want your knn to be trained
+#define SY 30
+
+void createInputVec(Mat_<float>,Mat_<int>);		// convert training images to vectors
+Mat trainPrePos(Mat);		// apply some preprocessing to img before feeding then to knn
+int knnPrePos(Mat,Mat&);	// for testing data
